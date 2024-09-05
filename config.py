f779836cb02a44022bfef776bbcf8cf735c1af70
rohitverma101dot <priya69137@gmail.com>
2024-09-05 08:24:24 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7425120105:AAFL6qGjfnMEPtZTr34W5B2L0wf4U4RQEys")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("Helix_33")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_ID=("5162770312")
+
+
+
+
+
+
+
+  
